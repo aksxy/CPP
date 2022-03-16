e259353902fa889ff5593fbde4fb207cc717a206
AKSHAY MULE <84891959+aksxy@users.noreply.github.com>
2022-03-16 23:50:24 +0530

    multiplication table

---
+++ Loops/display the multiplication table.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+main()
+{
+    int i,n,t;
+    cout<<"Enter Which Table you want = ";
+    cin>>n;
+    for(i=1;i<=10;i++)
+    {
+        t=n*i;
+        cout<<"15 * 10 = "<<t<<endl;
+    }
+}
