a14fc40d762e918e68c87bad93c797a727f9f574
aksxy <akshaymule080@gmail.com>
2022-04-11 04:51:42 +0530

    Convert Miles To Kilomiters

---
+++ Programming book drils/ConvertMilesIntoKilomiters.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int kilomiter,miles;
+    cout<<"Enter How much miles you need to convert into kilomiters : ";
+    cin>>miles;
+    cout<<miles<<" miles = "<<1.609*miles<<" kilomiters!";
+}
+
