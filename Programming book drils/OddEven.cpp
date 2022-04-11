cf482fde45a903b7563821ac0302777ab1726e13
aksxy <akshaymule080@gmail.com>
2022-04-12 04:33:48 +0530

    OddEven-no

---
+++ Programming book drils/OddEven.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int val,divid;
+    cout<<"Enter a number : ";
+    cin>>val;
+    if(val%2==0)
+        cout<<val<<" is a even number!";
+    else
+        cout<<val<<" is a odd number!";
+
+}
+
