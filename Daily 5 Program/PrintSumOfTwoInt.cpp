b95666238a523794404a2ce442c5a6abe3627bff
aksxy <akshaymule080@gmail.com>
2022-04-13 09:08:43 +0530

    Print Sum Of Two Int

---
+++ Daily 5 Program/PrintSumOfTwoInt.cpp
@@ -0,0 +1,10 @@
+//2.Write a program in C++ to print the sum of two numbers.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"The Sum of 10 and 20 is = "<<10+20;
+    return 0;
+}
+
