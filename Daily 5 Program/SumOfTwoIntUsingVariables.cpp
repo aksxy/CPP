4937e52a1e54040dc8d95c4d090c9c275b6f5ab0
aksxy <akshaymule080@gmail.com>
2022-04-16 11:28:48 +0530

    Sum of two int variables

---
+++ Daily 5 Program/SumOfTwoIntUsingVariables.cpp
@@ -0,0 +1,10 @@
+//4.Write a program in C++ to print the sum of two numbers using variables.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=12,b=13;
+    cout<<"The sum of "<<a<<" and "<<b<<" is : "<<a+b;
+}
+
