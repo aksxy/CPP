f932da20df16a085bda6f1e362e4cf83d0591a05
AKSHAY MULE <84891959+aksxy@users.noreply.github.com>
2022-04-18 11:24:14 +0530

    add two numbers accept through keyboard

---
+++ Daily 5 Program/add two numbers accept through keyboard.cpp
@@ -0,0 +1,14 @@
+//12.Write a program in C++ to add two numbers accept through keyboard.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int val1,val2;
+    cout<<"Input 1st number : ";
+    cin>>val1;
+    cout<<"Input 2nd number : ";
+    cin>>val2;
+    cout<<"The sum of the numbers is : "<<val1+val2<<'\n';
+    return 0;
+}
+
